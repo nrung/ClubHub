7ed37550e27d1805ff3653710f41002bfa0ba5de
Nick Rung <nrung@optonline.com>
2015-11-15 03:45:51 -0500

    Fixed some routes, signup, and login

--- app/routes.js
+++ app/routes.js
@@ -64,12 +64,12 @@ module.exports = function(app, passport) {
   }));
 
   // =====================================
-  // PROFILE SECTION =========================
+  // PROFILE SECTION =====================
   // =====================================
   // we will want this protected so you have to be logged in to visit
   // we will use route middleware to verify this (the isLoggedIn function)
-  app.get('/profile', isLoggedIn, function(req, res) {
-    res.render('profile.ejs', {
+  app.get('/locator', isLoggedIn, function(req, res) {
+    res.render('locator.ejs', {
       user : req.user // get the user out of session and pass to template
     });
   });
